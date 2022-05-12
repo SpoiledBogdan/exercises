9393c4be05be58966222588e039ee4bcd477fc51
Nikita Pustovoit <52323913+SpoiledBogdan@users.noreply.github.com>
2022-05-12 23:05:40 +0300

    Update 1_14.c

--- 1_14.c
+++ 1_14.c
@@ -1,25 +1,43 @@
-#include<stdio.h>
-#define LEN 12
+#include <stdio.h>
+#define LEN 10
 
 int main(void) {
-	int c, i, j, space, other;
+	int c, space, tab, newl, chars;
+	space = tab = newl = chars = 0;
 	int digit[LEN];
 
-	for (i = 0; i < LEN; ++i)
+	for (int i = 0; i < LEN; i++) {
 		digit[i] = 0;
-
-	while ((c = getchar()) != EOF) {
-		if (c >= '0' && c <= '9')
-			++digit[c - '0'];
-		else if (c == ' ' || c == '\t' || c == '\n')
-			++digit[11];
-		else
-			++digit[12];
 	}
 
-	for (i = 0; i < LEN; ++i){
-        printf("%d : ", i);
-        for (j = 1; j <= digit[i]; ++j)
-            printf("-");
-        printf("> %d\n", digit[i]); }
+	while ((c = getchar()) != EOF) {
+		if (c >= '0' && c <= '9') {
+			++digit[c - '0'];
+		}
+
+		else if (c == ' ') {
+			++space;
+		}
+
+		else if (c == '\t') {
+			++tab;
+		}
+
+		else if (c == '\n') {
+			++newl;
+		}
+
+		else {
+			++chars;
+		}
+	}
+
+	printf("digits:\n");
+	for (int i = 0; i < LEN; i++) {
+		printf("count of %d - %d\n", i, digit[i]);
+	}
+
+	printf("space - %d\ntabs - %d\nnew line - %d\nother - %d\n", space, tab, newl, chars);
+
+	return 0;
 }
